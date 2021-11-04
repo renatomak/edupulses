a7957a7134238ff9e33ef4e648f9e5cf6175ab1d
renatomak <rento.mark.silva@gmail.com>
2021-11-04 18:36:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Listening to the door ${PORT}`);
+});
---
+++ src/Routers.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/api/v1/users/fibonacci/:parameter", (req, res) => {});
+router.get("/api/v1/users/cyty/:parameter", (req, res) => {});
+
+module.exports = router;
